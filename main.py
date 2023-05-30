5c76697297f03128eb3480c7f50909fa526e32ca
HyeonjaeLee <guswo0329@gmail.com>
2023-05-31 00:29:49 +0900

    merge conflict 처리 오류수정,충돌처리를 위한 인자전달

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame#라이브러리
 import tank
-=======
 import block
 pygame.init()#초기화
 
@@ -10,17 +9,15 @@ screen=pygame.display.set_mode(background) #screen을 지정한 해상도로
 done=False #게임이 끝났는가?
 clock=pygame.time.Clock()
 tank1=tank.Tank()
-
+    
 blocks = pygame.sprite.Group() #스프라이트의 묶음으로
 
 for _ in range(10): #테스트용 10개만
-    block_obj = block.Block()
-    blocks.add(block_obj)
+    blockObj = block.Block()
+    blocks.add(blockObj)
 
 def running():
     global done,tank1 # 케릭터와 게임오버여부 전역변수로
-    x=50
-    y=50
     while not done:
         clock.tick(30) #30프레임
         screen.fill(backgroundColor)
@@ -29,7 +26,7 @@ def running():
             if event.type == pygame.QUIT:
                 done=True
 
-        tank1.update(pygame.key.get_pressed())# 키 입력을 받은대로 tank1에 보낸다.ㅁ
+        tank1.update(pygame.key.get_pressed(),blocks)# 키 입력을 받은대로 tank1에 보낸다.ㅁ
         screen.blit(tank1.image,tank1.rect)#탱크렌더링        
         blocks.draw(screen) #블럭묶음 렌더링   
         pygame.display.update()
