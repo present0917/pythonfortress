cb784c4c26fae96a7357320b15e0ac6a210452ed
HyeonjaeLee <guswo0329@gmail.com>
2023-05-31 02:45:48 +0900

    플레이어를 둘로 만들어서 탄환이 바닥에 떨어지면 차례가 넘어가도록 구현.

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ background = [800, 600]  # 배경 해상도
 screen = pygame.display.set_mode(background)  # screen을 지정한 해상도로
 done = False  # 게임이 끝났는가?
 clock = pygame.time.Clock()
-tank1 = tank.Tank()
-
+tanks = [tank.Tank(1), tank.Tank(2)]  # 탱크가 두개가됐다.
+nowPlayer=0 #0번플레이어부터 시작
 blocks = pygame.sprite.Group()  # 스프라이트의 묶음으로
 
 for _ in range(10):  # 테스트용 10개만
@@ -22,7 +22,7 @@ theBullet = None  # 탄환변수
 
 def running():
 
-    global done, tank1, theBullet  # 캐릭터와 게임오버여부,탄환 전역변수로
+    global done, tanks, theBullet,nowPlayer  # 캐릭터와 게임오버여부,탄환 전역변수로
     while not done:
         clock.tick(30)  # 30프레임
         screen.fill(backgroundColor)
@@ -31,11 +31,14 @@ def running():
             if event.type == pygame.QUIT:
                 done = True
 
-        newBullet = tank1.update(pygame.key.get_pressed())  # 키 입력을 받은대로 tank1에 보낸다.
+        newBullet = tanks[nowPlayer].update(pygame.key.get_pressed(),blocks)  # 키 입력을 받은대로 탱크에 보낸다.
         if newBullet:
             theBullet = newBullet  # 새 탄환
         
-        screen.blit(tank1.image, tank1.rect)  # 탱크렌더링
+        for i,tank in enumerate(tanks):  # 
+            screen.blit(tank.image, tank.rect)  # 탱크렌더링
+        
+        
         
         blocks.draw(screen)  # 블럭묶음 렌더링   
 
@@ -43,8 +46,16 @@ def running():
             theBullet.update()
             if theBullet.rect.y >= background[1]:  # 탄환이 바닥에 닿으면
                 theBullet = None  # 탄환을제거
+                nowPlayer = (nowPlayer + 1) % 2  # 턴넘김
             else:
-                screen.blit(theBullet.image, theBullet.rect)  # 탄환렌더링
+                for i, tank in enumerate(tanks):  # hit체크
+                    if theBullet.rect.colliderect(tank.rect) and i != nowPlayer:  # 상대에 탄맞으면
+                        tank.hiten()  # 체력까기
+                        theBullet = None  # 탄환제거
+                        nowPlayer = (nowPlayer + 1) % 2  # 턴넘김
+                        break
+                if theBullet:  # 아직탄환 날라가는중이면
+                    screen.blit(theBullet.image, theBullet.rect)  # 렌더링
                 
         pygame.display.update()
 
