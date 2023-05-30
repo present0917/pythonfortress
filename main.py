f25a65eb08788e4a4797b0e9fe3d0d05dd5f9d36
HyeonjaeLee <guswo0329@gmail.com>
2023-05-30 23:01:01 +0900

    block 클래스, 색은 지정, 화면크기 고려해서 랜덤한위치에 생성됨

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame#라이브러리
+import tank
 pygame.init()#초기화
 
 backgroundColor=(255,255,255)#배경색지정
@@ -6,8 +7,7 @@ background=[800,600] #배경 해상도
 screen=pygame.display.set_mode(background) #screen을 지정한 해상도로
 done=False #게임이 끝났는가?
 clock=pygame.time.Clock()
-tank1=pygame.image.load('images/tank/missile.png')
-tank1=pygame.transform.scale(tank1,(80,60))
+tank1=tank.Tank()
 
 def running():
     global done,tank1 # 케릭터와 게임오버여부 전역변수로
@@ -20,7 +20,10 @@ def running():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done=True
-        screen.blit(tank1,(x,y))#탱크렌더링        
+
+        tank1.update(pygame.key.get_pressed())# 키 입력을 받은대로 tank1에 보낸다.
+
+        screen.blit(tank1.image,tank1.rect)#탱크렌더링        
         pygame.display.update()
 
 
